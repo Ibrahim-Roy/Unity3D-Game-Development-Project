3612d44231b75239515068fff4e46a80de9af563
Ibrahim-Roy <ibrahimbadarroy@gmail.com>
2021-10-30 17:23:08 +0100

    New system for resource collection

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,31 @@ public class PlayerController : MonoBehaviour
 {
     public Vector2 moveValue ;
     public float speed ;
+    private GameObject collisionObject ;
 
     void OnMove(InputValue value) {
         moveValue = value.Get<Vector2>() ;
     }
 
-    void FixedUpdate () {
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.E)) {
+            if (collisionObject != null){
+                if (collisionObject.tag == "Tree") {
+                    collisionObject.transform.parent.gameObject.GetComponent<Tree>().takeDamage();
+                }
+            }
+        }
+    }
+
+    void FixedUpdate() {
         GetComponent <Rigidbody>().velocity = new Vector3(moveValue.x*speed , 0.0f , moveValue.y*speed) ;
+    }
+
+    void OnTriggerEnter(Collider other) {
+        collisionObject = other.GetComponent<Collider>().gameObject ;
+    }
+
+    void OnTriggerExit(Collider other) {
+        collisionObject = null ;
     }
 }
