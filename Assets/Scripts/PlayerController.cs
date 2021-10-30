033fdf3a4819922a141050dfdae887978a177fc6
Ibrahim-Roy <ibrahimbadarroy@gmail.com>
2021-10-31 00:54:22 +0100

    PLayer movement updated, aim with mouse, more animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,17 +7,28 @@ public class PlayerController : MonoBehaviour
 {
     public Vector2 moveValue ;
     public float speed ;
+    public Animator anim ;
     private GameObject collisionObject ;
+    [SerializeField] private LayerMask groundMask ;
 
     void OnMove(InputValue value) {
         moveValue = value.Get<Vector2>() ;
     }
 
     void Update() {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition) ;
+            Vector3 mousePosition = Vector3.zero ;
+            if (Physics.Raycast(ray, out RaycastHit raycastHit, groundMask)) {
+                mousePosition = raycastHit.point ;
+            }
+            Vector3 attackDirection = (mousePosition - transform.position).normalized ;
+            attackDirection.y = 0f;
+            anim.SetFloat("Horizontal", attackDirection.x) ;
+            anim.SetFloat("Vertical", attackDirection.z) ;
         if (Input.GetKeyDown(KeyCode.E)) {
             if (collisionObject != null){
                 if (collisionObject.tag == "Tree") {
-                    collisionObject.transform.parent.gameObject.GetComponent<Tree>().takeDamage();
+                    collisionObject.transform.parent.gameObject.GetComponent<Tree>().takeDamage() ;
                 }
             }
         }
