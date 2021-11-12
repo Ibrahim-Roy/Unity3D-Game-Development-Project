5dc47a7116564347332bb58da22011e9c731108e
mekctrl <miki1wt@gmail.com>
2021-11-12 00:01:51 +0000

    Combination with UI prefab and script

---
+++ Assets/Scripts/UserInterface.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserInterface : MonoBehaviour
+{
+    public Text HealthOutput;
+    public Text LogOutput;
+    public Text StoneOutput;
+
+    public Text Death;
+
+    float health;
+    int log = 0;
+    int stone = 0;
+
+
+    void Update()
+    {
+        deathDetect();
+        HealthOutput.text = "Health: " + health;
+        LogOutput.text = "Logs collected: " + log;
+        StoneOutput.text = "Stones collected: " + stone;
+    }
+
+    public void deathDetect()
+    {
+        if (health <= 0){
+             Death.text = "YOU DIED";
+        }
+    }
+
+    public void SetHealth(float _health)
+    {
+        health = _health;
+    }
+
+    public void SetLog(int _log)
+    {
+        log = _log;
+    }
+
+    public void SetStone(int _stone)
+    {
+        stone = _stone;
+    }
+
+}
